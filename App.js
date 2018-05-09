e27d96389283c12ebb2386296047bc841bb8cee7
Mark Molendijk <mark.molendijk@hotmail.com>
2018-05-09 10:03:06 +0200

    Update App.js
    Remove keys

--- App.js
+++ App.js
@@ -4,12 +4,7 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC5tN7JXHiRzOrBc34NR212AJ9_NRpGMyo",
-    authDomain: "mobileproject-cdee4.firebaseapp.com",
-    databaseURL: "https://mobileproject-cdee4.firebaseio.com",
-    projectId: "mobileproject-cdee4",
-    storageBucket: "mobileproject-cdee4.appspot.com",
-    messagingSenderId: "779119334300"
+// keys here
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
